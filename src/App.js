71fa6b0d7a0d84136a6b61cf19e3ef5f2f799b8a
shaik <shaikiko12@gmail.com>
2021-08-18 13:56:28 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+// class App extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {count:0}
+//   }
+
+//   render(){
+  
+//     return(
+//       <div>
+//         You clicked {this.state.count} times <br/>
+//         <button onClick={() => this.setState({count:this.state.count +1})}>
+//           Click here
+//         </button>
+//       </div>
+//     )
+//   }
+// }
+
+// same result with useState
+
+function Example() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState({
+    lat:"",
+    lon:""
+  })
+  
+
+  async function getIss(e) {
+    e.preventDefault();
+    const data = await fetch("https://api.wheretheiss.at/v1/satellites/25544"
+    ).then((res) => res.json())
+    .then((data) => data)
+
+    setData({lat:data.latitude, lon:data.longitude})
+            
+            
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      {/* <p>You clicked {count} times</p>
+      <button onClick = {() => setCount(count + 1)}>
+        Click here
+      </button> <br /> */}
+
+
+      <p>Where is the iss?</p>
+      
+      <button className="getiss" onClick={(e) => getIss(e)}>
+        Click to find out
+      </button>
+      <p>londitude: {data.lon}</p>
+      <p>latitude: {data.lat}</p>
+      
     </div>
-  );
+
+    </>
+  )
 }
 
-export default App;
+
+
+
+export default Example;
